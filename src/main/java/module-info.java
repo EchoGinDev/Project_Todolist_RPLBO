eaf346d7033b24300d863f5517d2bbb7e40733ef
EmperorGinzz01 <124578382+EchoGinDev@users.noreply.github.com>
2025-05-31 20:59:17 +0700

    commit 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,13 @@ module org.example.week10 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
     opens org.example.week10 to javafx.fxml;
     exports org.example.week10;
+    exports org.example.week10.Controller;
+    opens org.example.week10.Controller to javafx.fxml;
+    exports org.example.week10.Manager;
+    opens org.example.week10.Manager to javafx.fxml;
 }
 
